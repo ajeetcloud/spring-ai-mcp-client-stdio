58c54767f9f7ab14169ecb009853ceb8c41947a8
Ajeet Singh <ajeet.cloud@gmail.com>
2025-06-01 23:06:07 +0530

    Initial commit

---
+++ src/main/java/org/springaimcpclientstdio/SpringAiMcpClientStdioApplication.java
@@ -0,0 +1,13 @@
+package org.springaimcpclientstdio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiMcpClientStdioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiMcpClientStdioApplication.class, args);
+    }
+
+}
